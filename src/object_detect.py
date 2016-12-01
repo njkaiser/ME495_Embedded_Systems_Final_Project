293a5367a912e7ecb59d1235045d88960f6f92de
Nate Kaiser <natejkaiser@gmail.com>
2016-11-30 21:00:32 -0600

    red detection added, parameters updated for Baxter's hand cameras

--- src/object_detect.py
+++ src/object_detect.py
@@ -31,7 +31,7 @@ class window_with_trackbars(object):
 
     def nothing(self, x):
         # 'do nothing' callback needed for trackbars operation
-        # might as well print out the trackbar value at least
+        # might as well print out the trackbar value
         print "value of trackbar is:", x
 
     def get_vals(self):
@@ -43,7 +43,7 @@ class window_with_trackbars(object):
 
 
 
-def filter_red(img_hsv):
+def filter_red(img_hsv, img_raw):
     # filter non-red objects from camera feed
     v = redwin.get_vals()
     low_vals = np.array([v[0], v[1], v[2]])
@@ -54,15 +54,14 @@ def filter_red(img_hsv):
     red = red_lo + red_hi
 
     # smooth the image a bit
-    n = 3
+    n = 2
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(n,n))
     red = cv2.morphologyEx(red, cv2.MORPH_OPEN, kernel)
     red = cv2.morphologyEx(red, cv2.MORPH_CLOSE, kernel)
 
     # now mask onto original image and return
-    red = cv2.bitwise_and(img_hsv, img_hsv, mask=red)
+    red = cv2.bitwise_and(img_raw, img_raw, mask=red)
     return red
-
 
 
 
@@ -70,15 +69,16 @@ def callback(ros_img):
     bridge = cv_bridge.CvBridge()
 
     # convert ROS image to openCV image for processing
-    img_raw = bridge.imgmsg_to_cv2(ros_img, desired_encoding="passthrough")
-    img_hsv = cv2.cvtColor(img_raw, cv2.COLOR_BGR2HSV)
+    img_raw = bridge.imgmsg_to_cv2(ros_img, desired_encoding="bgr8")
     img_blur = cv2.GaussianBlur(img_raw, (5,5), 1)
+    img_hsv = cv2.cvtColor(img_blur, cv2.COLOR_BGR2HSV)
     img_canny = cv2.Canny(img_blur, cannywin.get_vals()[0], cannywin.get_vals()[1])
 
-    image_red = filter_red(img_hsv)
+    image_red = filter_red(img_hsv, img_raw)
 
     # cv2.imshow("image_blur", img_blur)
     cv2.imshow("image_canny", img_canny)
+    # cv2.imshow("image_hsv", img_hsv)
     cv2.imshow("image_red", image_red)
     cv2.waitKey(10)
 
@@ -93,17 +93,21 @@ if __name__ == '__main__':
         # set up initial variables:
         rospy.init_node('object_detect')
 
+
+        # create window and set up necessary thresholds:
         canny_tb_highs = [1000, 1000]
-        canny_tb_defaults = [200, 120]
+        # canny_tb_defaults = [200, 120] # for camera
+        canny_tb_defaults = [110, 110] # for Baxter hand camera
         cannywin = window_with_trackbars('image_canny', canny_tb_defaults, canny_tb_highs)
 
+        # these values tuned on baxter near fume hood, right hand camera
         red_tb_highs = [179, 255, 255, 179, 255, 255, 179, 255, 255, 179, 255, 255]
-        red_tb_defaults = [0, 0, 0, 35, 50, 255, 170, 0, 0, 179, 255, 255]
+        red_tb_defaults = [0, 130, 70, 10, 230, 255, 170, 0, 0, 179, 255, 255]
         redwin = window_with_trackbars('image_red', red_tb_defaults, red_tb_highs)
 
         # subscribe to whatever image topic we want to use for object detection
-        rospy.Subscriber("/usb_cam/image_raw", Image, callback) # testing with webcam
-        # rospy.Subscriber("/cameras/left_hand_camera/image", Image, callback)
+        # rospy.Subscriber("/usb_cam/image_raw", Image, callback) # testing with webcam
+        rospy.Subscriber("/cameras/right_hand_camera/image", Image, callback)
 
         # keep program open until we're good and done with it
         rospy.spin()
