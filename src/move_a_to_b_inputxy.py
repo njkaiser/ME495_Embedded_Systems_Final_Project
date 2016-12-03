350408052123760c26f57d8cc4c779ac6277dc98
Nate Kaiser <natejkaiser@gmail.com>
2016-12-02 21:31:16 -0600

    works now to do linear sweep of table - need to copy/paste this code into scan_initial.py

--- src/move_a_to_b_inputxy.py
+++ src/move_a_to_b_inputxy.py
@@ -13,19 +13,22 @@ from baxter_core_msgs.srv import (
     SolvePositionIKRequest,
 )
 import numpy as np
-import ang_calc_lib as angcalc
+# from ~/ME495_Final_Project/ws/src/baxter_interface/src/joint_trajectory_action/joint_trajectory_action.py import JointTrajectoryActionServer
+
+
 
 def main():
 
+    side = 'right'
 
     rospy.init_node("joint_move_cartesian")
     #Create a publisher
-    left = baxter_interface.Limb('left')
+    arm = baxter_interface.Limb(side)
 
     # Call service
-    ns_l = "ExternalTools/" + 'left' + "/PositionKinematicsNode/IKService"
-    iksvc_l = rospy.ServiceProxy(ns_l, SolvePositionIK)
-    ikreq_l = SolvePositionIKRequest()
+    ns = "ExternalTools/" + side + "/PositionKinematicsNode/IKService"
+    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+    ikreq = SolvePositionIKRequest()
 
     # Customize message
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
@@ -56,92 +59,112 @@ def main():
     #         ),
     #     ),
     # }
-    
+
     # specify x and y
     # The approximately possible bound of x and y for left arm at z = -0.7
     # ----> x : [0.64,1.06]
     # ----> y : [0.01,0.53]
-    x = 0.8
-    y = 0.1
+    # y = 0.1
 
-    #z-axis and orientation must be kept in this form for baxter to reach outward
-    poses = {
-        'left': PoseStamped(
-            header=hdr,
-            pose=Pose(
-                position=Point(
-                    x= x,
-                    y= y,
-                    z= -0.07,
-                ),
-                orientation=Quaternion(
-                    x= 0.71,
-                    y= 0.0,
-                    z= 0.71,
-                    w=-0.0,
-                ),
-            ),
-        ),
-    }
-    x_b4 = 0.839724445993
-    ikreq_l.pose_stamp.append(poses['left'])
-    try:
-        rospy.wait_for_service(ns_l, 5.0)
-        resp_l = iksvc_l(ikreq_l)
-    except (rospy.ServiceException, rospy.ROSException), e:
-        rospy.logerr("Service call failed: %s" % (e,))
-        return 
-    print resp_l
-    if (resp_l.isValid[0]):
-        print("LEFT_SUCCESS - Valid Joint Solution Found:")
-        # Format solution into Limb API-compatible dictionary
-        limb_joints_l = dict(zip(resp_l.joints[0].name, resp_l.joints[0].position))
-        print limb_joints_l
-    else:
-        print("LEFT_INVALID POSE - No Valid Joint Solution Found.")
-    left.move_to_joint_positions(limb_joints_l)
+    # jtas = JointTrajectoryActionServer(side, reconfig_server, rate=100.0,
+    #          mode='position_w_id'):
 
-    # rospy.sleep(5.)
+    arm.set_joint_position_speed(0.1) # % of max speed
+
+    x = 0.9
+    y = -0.45
+    z = 0.0
+
+    for i, y in enumerate(np.linspace(-0.45, 0.2, 1000, endpoint=True)):#[-0.4, 0.2]:
+        # for z in [0.0, 0]:
+            print "x, y, z", x, y, z
+            #z-axis and orientation must be kept in this form for baxter to reach outward
+            poses = {
+                side: PoseStamped(
+                    header=hdr,
+                    pose=Pose(
+                        position=Point(
+                            x= x,
+                            y= y,
+                            z= z,
+                        ),
+                        orientation=Quaternion(
+                            x = 0.0,
+                            y = 0.71,
+                            z = 0.0,
+                            w = 0.71,
+                        ),
+                    ),
+                ),
+            }
+            # x_b4 = 0.839724445993
+
+            ikreq.pose_stamp = [poses[side]]
+
+            try:
+                # rospy.wait_for_service(ns, 5.0)
+                resp = iksvc(ikreq)
+            except (rospy.ServiceException, rospy.ROSException), e:
+                rospy.logerr("Service call failed: %s" % (e,))
+                return
+            # print resp
+            if (resp.isValid[0]):
+                # print("LEFT_SUCCESS - Valid Joint Solution Found:")
+                # Format solution into Limb API-compatible dictionary
+                limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
+                if i == 0:
+                    arm.set_joint_position_speed(0.5) # % of max speed
+                    arm.move_to_joint_positions(limb_joints)
+                else:
+                    arm.set_joint_position_speed(0.1) # % of max speed
+                    arm.set_joint_positions(limb_joints)
+                # print limb_joints
+                rospy.sleep(0.005)
+            else:
+                print("LEFT_INVALID POSE - No Valid Joint Solution Found.")
+
+
+
 
     # move outward more 5 cm in x-dir ----> Sill
     # z-axis and orientation must be kept in this form
-    poses = {
-        'left': PoseStamped(
-            header=hdr,
-            pose=Pose(
-                position=Point(
-                    x= x_b4+0.05,
-                    y= 0.278189662246,
-                    z= -0.07,
-                ),
-                orientation=Quaternion(
-                    x= 0.71,
-                    y= 0.0,
-                    z= 0.71,
-                    w=-0.0,
-                ),
-            ),
-        ),
-    }
-    ikreq_l = SolvePositionIKRequest()
-
-    ikreq_l.pose_stamp.append(poses['left'])
-    try:
-        rospy.wait_for_service(ns_l, 5.0)
-        resp_l = iksvc_l(ikreq_l)
-    except (rospy.ServiceException, rospy.ROSException), e:
-        rospy.logerr("Service call failed: %s" % (e,))
-        return 
-    
-    if (resp_l.isValid[0]):
-        print("LEFT_SUCCESS - Valid Joint Solution Found:")
-        # Format solution into Limb API-compatible dictionary
-        limb_joints_l = dict(zip(resp_l.joints[0].name, resp_l.joints[0].position))
-        print limb_joints_l
-    else:
-        print("LEFT_INVALID POSE - No Valid Joint Solution Found.")
-
-    left.move_to_joint_positions(limb_joints_l)
+    # poses = {
+    #     'left': PoseStamped(
+    #         header=hdr,
+    #         pose=Pose(
+    #             position=Point(
+    #                 x= x_b4+0.05,
+    #                 y= 0.278189662246,
+    #                 z= -0.07,
+    #             ),
+    #             orientation=Quaternion(
+    #                 x= 0.71,
+    #                 y= 0.0,
+    #                 z= 0.71,
+    #                 w=-0.0,
+    #             ),
+    #         ),
+    #     ),
+    # }
+    # ikreq_l = SolvePositionIKRequest()
+    #
+    # ikreq_l.pose_stamp.append(poses['left'])
+    # try:
+    #     rospy.wait_for_service(ns_l, 5.0)
+    #     resp_l = iksvc_l(ikreq_l)
+    # except (rospy.ServiceException, rospy.ROSException), e:
+    #     rospy.logerr("Service call failed: %s" % (e,))
+    #     return
+    #
+    # if (resp_l.isValid[0]):
+    #     print("LEFT_SUCCESS - Valid Joint Solution Found:")
+    #     # Format solution into Limb API-compatible dictionary
+    #     limb_joints_l = dict(zip(resp_l.joints[0].name, resp_l.joints[0].position))
+    #     print limb_joints_l
+    # else:
+    #     print("LEFT_INVALID POSE - No Valid Joint Solution Found.")
+    #
+    # left.move_to_joint_positions(limb_joints_l)
 
 if __name__ == '__main__':
     main()
