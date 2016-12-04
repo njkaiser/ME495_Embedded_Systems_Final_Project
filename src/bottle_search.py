27ce81017d94904d70694ae3f3720af2761dc1f0
Nate Kaiser <natejkaiser@gmail.com>
2016-12-04 01:32:02 -0600

    works to the first target, need to work out the algorithm to ignore the first target and look for the 2nd

--- src/bottle_search.py
+++ src/bottle_search.py
@@ -19,74 +19,89 @@ from baxter_core_msgs.msg import EndpointState
 
 
 class Position(object):
-    def __init__(x, y, z):
+    def __init__(self, x, y, z):
         self.x = x # FORWARD/BACK, + is Baxter's forward
         self.y = y # LEFT/RIGHT, + is Baxter's left
         self.z = z # VERTICAL, + is up
-        
+
     def get_point(self):
-        a = Point()
-        a.x = self.x
-        a.y = self.y
-        a.z = self.z
-        return a 
+        p = Point()
+        p.x = self.x
+        p.y = self.y
+        p.z = self.z
+        return p
 
 
-def bottle_search(start, end):
-    stop = False
-    # Customize message
+rospy.init_node("joint_move_cartesian")
+
+side = 'right'
+arm = baxter_interface.Limb(side)
+
+xyz = Position(0., 0., 0.)
+
+xyz_pres = Position(0., 0., 0.)
+
+ns = "ExternalTools/" + side + "/PositionKinematicsNode/IKService"
+iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
+ikreq = SolvePositionIKRequest()
+
+
+
+def bottle_search(start, end, side):
+    # stop = False
     hdr = Header(stamp=rospy.Time.now(), frame_id='base')
-    for i, y in enumerate(np.linspace(-0.45, 0.2, 500, endpoint=True)):#[-0.4, 0.2]:
-        print "X = ", xyz.x
-            # if (xyz.x == 0.0):
-               # rospy.sleep(10.)
-            #z-axis and orientation must be kept in this form for baxter to reach outward
-        if not stop: 
-            poses = {
-                side: PoseStamped(
-                    header=hdr,
-                    pose=Pose(
-                        position=Point(
-                            x= x,
-                            y= y,
-                            z= z,
-                        ),
-                        orientation=Quaternion(
-                            x = 0.0,
-                            y = 0.71,
-                            z = 0.0,
-                            w = 0.71,
-                        ),
+    y_start = start.y
+    y_end = end.y
+    dy = abs(y_start - y_end)
+    npoints = int(dy/0.003) # SWAG
+    x = start.x
+    z = start.z
+    for i, y in enumerate(np.linspace(y_start, y_end, npoints, endpoint=True)):
+        # print "X = ", xyz.x
+        # if not stop:
+        pose = {
+            side: PoseStamped(
+                header=hdr,
+                pose=Pose(
+                    position=Point(
+                        x= x,
+                        y= y,
+                        z= z,
+                    ),
+                    # z-axis and orientation must be kept in this form for baxter to reach outward
+                    orientation=Quaternion(
+                        x = 0.0,
+                        y = 0.71,
+                        z = 0.0,
+                        w = 0.71,
                     ),
                 ),
-            }
-        else :
-           # print "not hereeeee"
-            poses = {
-                side: PoseStamped(
-                    header=hdr,
-                    pose=Pose(
-                        position=Point(
-                            x= xyz_pres.x,
-                            y= xyz_pres.y,
-                            z= xyz_pres.z,
-                        ),
-                        orientation=Quaternion(
-                            x = 0.0,
-                            y = 0.71,
-                            z = 0.0,
-                            w = 0.71,
-                        ),
-                    ),
-                ),
-            }
-            
+            ),
+        }
+        # else :
+        #    # print "not hereeeee"
+        #     pose = {
+        #         side: PoseStamped(
+        #             header=hdr,
+        #             pose=Pose(
+        #                 position=Point(
+        #                     x= xyz_pres.x,
+        #                     y= xyz_pres.y,
+        #                     z= xyz_pres.z,
+        #                 ),
+        #                 orientation=Quaternion(
+        #                     x = 0.0,
+        #                     y = 0.71,
+        #                     z = 0.0,
+        #                     w = 0.71,
+        #                 ),
+        #             ),
+        #         ),
+        #     }
 
-        ikreq.pose_stamp = [poses[side]]
- 
+        ikreq.pose_stamp = [pose[side]]
 
         try:
-            #
             resp = iksvc(ikreq)
         except (rospy.ServiceException, rospy.ROSException), e:
             rospy.logerr("Service call failed: %s" % (e,))
@@ -97,34 +112,35 @@ def bottle_search(start, end):
             # Format solution into Limb API-compatible dictionary
             limb_joints = dict(zip(resp.joints[0].name, resp.joints[0].position))
             if i == 0:
-                arm.set_joint_position_speed(0.5) # % of max speed
+                arm.set_joint_position_speed(0.6) # % of max speed
                 arm.move_to_joint_positions(limb_joints)
             else:
-                arm.set_joint_position_speed(0.1) # % of max speed
+                arm.set_joint_position_speed(0.25) # % of max speed
                 arm.set_joint_positions(limb_joints)
-                if stop:
+                # if stop:
                     # x_is_zero = True
                     # if x_is_zero:
                     # bottle_xyz.append(xyz_pres.get_point())
                     # x_is_zero = True
-                    
-                    #     can_count == False
-                    # if 
-                    print "Bottle_xyz : ", bottle_xyz
-                    return xyz_pres.get_point()
-                    
+
+                    # can_count == False
+                    # if
+                    # print "Bottle_xyz : ", bottle_xyz
+                    # return xyz_pres.get_point()
+
                     # ROSPY.SLEEP(5)
-                    
+
                 print "value",  xyz.x
                 if xyz.x == 0.0:
-                    stop = True
-                    
+                    # stop = True
+                    return xyz_pres.get_point()
+
                 # print limb_joints
-                rospy.sleep(0.0025)
+                rospy.sleep(0.001)
         else:
             print("LEFT_INVALID POSE - No Valid Joint Solution Found.")
-    
-    return 
+
+    return
 
 
 def get_present_state(data, xyz_pres):
@@ -150,31 +166,22 @@ def move_callback(data, xyz):
 def main():
 
 
-    side = 'right'
+
 
     # print "XYZ_PRES : ", xyz_pres
-    bottle_xyz = []
-    if len(bottle_xyz) == 3:
-        return
-    rospy.init_node("joint_move_cartesian")
+    # bottle_xyz = []
+    # if len(bottle_xyz) == 3:
+    #     return
     #Create a publisher
-    arm = baxter_interface.Limb(side)
 
-    xyz = Position()
 
-    xyz_pres = Position()
-
-    
 
     #Subscribe to image
-    rospy.Subscriber("/object_image_command", Point, move_callback, xyz, queue_size=1)
     rospy.Subscriber("/robot/limb/right/endpoint_state", EndpointState, get_present_state, xyz_pres, queue_size=1)
+    rospy.Subscriber("/object_image_command", Point, move_callback, xyz, queue_size=1)
 
     # Call service
-    ns = "ExternalTools/" + side + "/PositionKinematicsNode/IKService"
     rospy.wait_for_service(ns, 5.0)
-    iksvc = rospy.ServiceProxy(ns, SolvePositionIK)
-    ikreq = SolvePositionIKRequest()
 
     # jtas = JointTrajectoryActionServer(side, reconfig_server, rate=100.0,
     #          mode='position_w_id'):
@@ -189,14 +196,20 @@ def main():
     end_pos = Position(x, y_end, z)
 
     bottle_positions = []
+    for i in [1, 2]:
+        bp = bottle_search(start_pos, end_pos, side)
+        print bp
+        bottle_positions.append(bp)
+        start_pos = bp
+        start_pos.y += 0.07 # guess
 
-    bp = bottle_search(start_pos, end_pos)
-    print bp
+    print "FINAL POSITIONS APPENDED:"
+    print bottle_positions
 
 
     # move outward more 5 cm in x-dir ----> Sill
     # z-axis and orientation must be kept in this form
-    # poses = {
+    # pose = {
     #     'left': PoseStamped(
     #         header=hdr,
     #         pose=Pose(
@@ -216,7 +229,7 @@ def main():
     # }
     # ikreq_l = SolvePositionIKRequest()
     #
-    # ikreq_l.pose_stamp.append(poses['left'])
+    # ikreq_l.pose_stamp.append(pose['left'])
     # try:
     #     rospy.wait_for_service(ns_l, 5.0)
     #     resp_l = iksvc_l(ikreq_l)
