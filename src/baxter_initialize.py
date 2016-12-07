d4daffc33fd0c879aa44fd8b17abdcd77300a9e9
Nate Kaiser <natejkaiser@gmail.com>
2016-12-06 19:37:17 -0600

    initialize node now enables robot and calibrates grippers for use

--- src/baxter_initialize.py
+++ src/baxter_initialize.py
@@ -3,35 +3,25 @@
 import sys
 sys.dont_write_bytecode = True
 import rospy
-# import tf
 import baxter_interface
-# import os
-# import time
-# from moveit_commander import conversions
-# from geometry_msgs.msg import PoseStamped, Pose, Point, Quaternion
-# from std_msgs.msg import Header
-# import std_srvs.srv
-# from baxter_core_msgs.srv import SolvePositionIK, SolvePositionIKRequest
 
-
-def initialize():
-    # initialize Baxter
-    baxter_interface.RobotEnable().enable()
-
-
-
-    ### END initialize()
 
 
 if __name__ == '__main__':
     try:
         rospy.init_node('baxter_initialize')
 
-        initialize()
-        rospy.spin()
+        # enable robot
+        baxter_interface.RobotEnable().enable()
+
+        # calibrate grippers so we can use them later
+        for side in ['right', 'left']:
+            side = baxter_interface.Gripper(side)
+            side.calibrate()
 
     except:
-        print "\n/baxter_initialize node killed, DISABLING BAXTER\n"
-        baxter_interface.RobotEnable().disable()
+        # print "\n/baxter_initialize node killed, DISABLING BAXTER\n"
+        # baxter_interface.RobotEnable().disable()
+        pass
 
 ### END OF SCRIPT
